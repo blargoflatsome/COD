ab8c4b38051017a3cac597992269bdadc14271a5
blargoflatsome <jordan.buermann@gmail.com>
2025-11-11 23:54:32 -0800

    nuxt ui is working

---
+++ app/layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const { data: navigation } = await useAsyncData('navigation', () =>
+  queryCollectionNavigation('content')
+)
+
+</script>
+
+<template>
+  <UHeader toggle-side="left">
+    <template #title>Catacombs of Despair</template>
+		<UDrawer>
+			<UButton label="Open" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up" />
+
+			<template #content>
+				<UContentNavigation :navigation="navigation" :default-open="false"  highlight highlight-color="primary" color="primary" variant="pill" />
+			</template>
+		</UDrawer>
+
+    <template #right>
+			Right Side
+    </template>
+  </UHeader>
+  <UPage>
+		<slot />
+  </UPage>
+</template>
--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,14 +1,15 @@
 <template>
-  <template v-if="page">
-    <ContentRenderer :value="page" />
-  </template>
-  <template v-else>
-    <div class="empty-page">
-      <h1>Page Not Found</h1>
-      <p>Oops! The content you're looking for doesn't exist.</p>
-      <NuxtLink to="/">Go back home</NuxtLink>
-    </div>
-  </template>
+  <UPage v-if="page">
+    <UPageHeader :title="page.title" />
+
+    <UPageBody>
+      <ContentRenderer v-if="page.body" :value="page" />
+    </UPageBody>
+
+    <template v-if="page?.body?.toc?.links?.length" #right>
+      <UContentToc :links="page.body.toc.links" />
+    </template>
+  </UPage>
 </template>
 
 <script lang="ts" setup>
