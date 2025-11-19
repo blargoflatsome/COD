78ecceeb91720751d4238c854a46fc908df1eda8
blargoflatsome <jordan.buermann@gmail.com>
2025-11-18 19:52:06 -0800

    placeholder is bad

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="w-screen relative left-1/2 -ml-[50vw] right-1/2 -mr-[50vw] max-w-screen h-auto">
-  <NuxtImg src="/images/mainBanner2.png" alt="catacombs" class="w-full h-auto" placeholder  />
+  <NuxtImg src="/images/mainBanner2.png" alt="catacombs" class="w-full h-auto"  />
 </div>
 
 
